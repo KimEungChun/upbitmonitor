e9469447015b2f4a9f2c82123c2ec5c2f99ee754
KimEungChun <kkimec@gmail.com>
2025-04-29 14:14:45 +0900

    그냥커밋

--- telegram_test.py
+++ telegram_test.py
@@ -1,3 +1,4 @@
+import asyncio
 from telegram import Bot
 
 # 아까 복사한 Bot Token
@@ -5,6 +6,9 @@ TOKEN = "7475326912:AAHdnqpXNyOiSclg56zFvqu3gTq3CDXexXU"
 # 아까 찾은 내 Chat ID
 CHAT_ID = 7692872494
 
-bot = Bot(token=TOKEN)
+async def main():
+    bot = Bot(token=TOKEN)
+    await bot.send_message(chat_id=CHAT_ID, text="🚀 Azure 서버에서 테스트 메시지 보냄!")
 
-bot.send_message(chat_id=CHAT_ID, text="🚀 Azure 서버에서 테스트 메시지 보냄!")
+if __name__ == "__main__":
+    asyncio.run(main())
